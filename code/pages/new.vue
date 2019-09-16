e73a6cf467d0fcf5d1616b2c2ef1da6a47fe15b7
fussy113 <s1220080@gmail.com>
2019-09-16 16:59:54 +0900

    axios実践

--- code/pages/new.vue
+++ code/pages/new.vue
@@ -1,12 +1,52 @@
 <template>
   <div class="container">
-    <h1>/new</h1>
+    <h1 class="title">
+      /new
+    </h1>
+    <h2 class="subtitle">
+      ここでAPIを呼ぶ
+    </h2>
+    <div>
+      <h3>
+        仕様
+      </h3>
+      <p>
+        ・QiitaのAPIを叩いてフォームに入力されたタグの記事を最新5件取得する。
+      </p>
+    </div>
+    <div class="columns field">
+      <div class="column is-5 control">
+        <input v-model="searchTagForm" class="input is-primary" type="text" placeholder="input">
+      </div>
+      <div class="column is-2 control">
+        <button class="button is-link" @click="fetchData">
+          検索
+        </button>
+      </div>
+    </div>
+    <div class="list is-hoverable">
+      <a v-for="(qiitaReport, index) in qiitaReports" :key="index + 1" :href="qiitaReport.url" class="list-item">
+        {{ qiitaReport.user.id }}さん : {{ qiitaReport.title }}
+      </a>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      searchTagForm: '',
+      qiitaReports: []
+    }
+  },
+  methods: {
+    async fetchData () {
+      // ここの文法を覚えてもらえば
+      const res = await this.$axios.$get('https://qiita.com/api/v2/tags/' + this.searchTagForm + '/items?per_page=5')
+      this.qiitaReports = res
+    }
+  }
 }
 </script>
 
